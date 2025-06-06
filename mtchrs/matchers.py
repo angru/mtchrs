2694b407cb2acdc76e3462a62a2e0f021c64d724
angru <angru@list.ru>
2025-06-07 00:32:44 +0300

    Refactor Matcher repr to be dynamic

--- mtchrs/matchers.py
+++ mtchrs/matchers.py
@@ -5,28 +5,34 @@ import typing as t
 
 
 class Matcher:
-    def __init__(self, func: t.Callable[[t.Any], bool], repr_str: str):
+    def __init__(self, func: t.Callable[[t.Any], bool], repr_func: t.Callable[[], str]):
         self._func = func
-        self._repr_str = repr_str
+        self._repr_func = repr_func
 
     def __eq__(self, other: t.Any) -> bool:
         return self._func(other)
 
     def __and__(self, other: Matcher) -> Matcher:
-        return Matcher(lambda v: self == v and other == v, f"({self}) & ({other})")
+        return Matcher(
+            lambda v: self == v and other == v,
+            lambda: f"({self}) & ({other})",
+        )
 
     def __or__(self, other: "Matcher") -> "Matcher":
-        return Matcher(lambda v: self == v or other == v, f"({self}) | ({other})")
+        return Matcher(
+            lambda v: self == v or other == v,
+            lambda: f"({self}) | ({other})",
+        )
 
     def __repr__(self) -> str:
-        return self._repr_str
+        return self._repr_func()
 
     def __invert__(self) -> Matcher:
-        return Matcher(lambda v: not self._func(v), f"~({self})")
+        return Matcher(lambda v: not self._func(v), lambda: f"~({self})")
 
     @staticmethod
     def any() -> Matcher:
-        return Matcher(lambda v: True, "Any")
+        return Matcher(lambda v: True, lambda: "Any")
 
     @staticmethod
     def eq() -> PersistentMatcher:
@@ -36,7 +42,7 @@ class Matcher:
     def type(*types: type) -> Matcher:
         return Matcher(
             lambda v: isinstance(v, types),
-            f'Type[{" | ".join((str(t) for t in types))}]',
+            lambda: f'Type[{" | ".join((str(t) for t in types))}]',
         )
 
     @staticmethod
@@ -44,7 +50,7 @@ class Matcher:
         compiled = re.compile(pattern) if isinstance(pattern, str) else pattern
         return Matcher(
             lambda v: isinstance(v, str) and bool(compiled.match(v)),
-            f"{compiled}",
+            lambda: f"{compiled}",
         )
 
 
@@ -52,16 +58,13 @@ class PersistentMatcher(Matcher):
     _no_value = object()
 
     def __init__(self):
-        super().__init__(self._func, "PersistentMatcher")
         self._value = self._no_value
+        super().__init__(self._func, lambda: f"PersistentMatcher(value={self._value!r})")
 
     def _func(self, other: t.Any) -> bool:
         if self._value is self._no_value:
             self._value = other
         return self._value == other
-
-    def __repr__(self) -> str:
-        return f"PersistentMatcher(value={self._value!r})"
 
 
 mtch = Matcher
