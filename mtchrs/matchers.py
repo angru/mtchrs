402bd88c9d1dd073d113073e44e6be05249b4f21
angru <angru@list.ru>
2025-06-06 21:59:21 +0300

    first version

---
+++ mtchrs/matchers.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import re
+import typing as t
+
+
+class Matcher:
+    def __init__(self, func: t.Callable[[t.Any], bool], repr_str: str):
+        self._func = func
+        self._repr_str = repr_str
+
+    def __eq__(self, other: t.Any) -> bool:
+        return self._func(other)
+
+    def __and__(self, other: Matcher) -> Matcher:
+        return Matcher(lambda v: self == v and other == v, f"({self}) & ({other})")
+
+    def __or__(self, other: "Matcher") -> "Matcher":
+        return Matcher(lambda v: self == v or other == v, f"({self}) | ({other})")
+
+    def __repr__(self) -> str:
+        return self._repr_str
+
+    def __invert__(self) -> Matcher:
+        return Matcher(lambda v: not self._func(v), f"~({self})")
+
+    @staticmethod
+    def any() -> Matcher:
+        return Matcher(lambda v: True, "Any")
+
+    @staticmethod
+    def eq() -> PersistentMatcher:
+        return PersistentMatcher()
+
+    @staticmethod
+    def type(*types: type) -> Matcher:
+        return Matcher(
+            lambda v: isinstance(v, types),
+            f'Type[{" | ".join((str(t) for t in types))}]',
+        )
+
+    @staticmethod
+    def regex(pattern: t.Union[str, re.Pattern]) -> Matcher:
+        compiled = re.compile(pattern) if isinstance(pattern, str) else pattern
+        return Matcher(
+            lambda v: isinstance(v, str) and bool(compiled.match(v)),
+            f"{compiled}",
+        )
+
+
+class PersistentMatcher(Matcher):
+    _no_value = object()
+
+    def __init__(self):
+        super().__init__(self._func, "PersistentMatcher")
+        self._value = self._no_value
+
+    def _func(self, other: t.Any) -> bool:
+        if self._value is self._no_value:
+            self._value = other
+        return self._value == other
+
+    def __repr__(self) -> str:
+        return f"PersistentMatcher(value={self._value})"
+
+
+mtch = Matcher
---
+++ tests/test_matchers.py
@@ -0,0 +1,20 @@
+from mtchrs.matchers import mtch
+
+
+def test_matcher() -> None:
+    assert mtch.any() == 123
+    assert mtch.type(int, float) == 3.14
+    assert mtch.regex(r"\d+") == "456"
+    assert mtch.type(int) | (mtch.type(str) & mtch.regex(r"\d+")) == 789
+    assert mtch.type(int) | (mtch.type(str) & mtch.regex(r"\d+")) == "789"
+
+    assert {"id": mtch.type(int), "child": {"id": mtch.any()}} == {
+        "id": 1,
+        "child": {"id": 2},
+    }
+
+
+def test_matcher_keeps_value() -> None:
+    user_id = mtch.eq()
+    assert {"id": 1, "child": {"id": 2}} != {"id": user_id, "child": {"id": user_id}}
+    assert {"id": 1, "child": {"id": 1}} == {"id": user_id, "child": {"id": user_id}}
